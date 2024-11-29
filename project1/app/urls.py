990806b70643a4bc67b9b1e56e8b3c46651b3204
vishnup0888 <vishnup08888@gmail.com>
2024-11-29 11:27:40 +0530

    file

--- project1/app/urls.py
+++ project1/app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('add/', views.add_data, name='add_data'),
-    path('update/<int:user_id>/', views.update_data, name='update_data'),
+path('',views.index),
+path('','update./int:slno'>,views.update),
 ]
--- project1/app/views.py
+++ project1/app/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+
 
 # Create your views here.
+subject=[]
+def index(request):
+    if request.method=='POST':
+        sub=request.POST['text']
+        sub.append({'sub:sub'})
+        return redirect(home)
+    return render(request,'index.html')
+def home(request):
+    return render(request,'index.html')
